50ba2d596b6aa6712a5cf5196f0c5e2dd9f59e66
Maga Jayesh <92910436+MagaJayesh@users.noreply.github.com>
2021-10-22 12:19:28 +0530

    Add files via upload

---
+++ guru raghavendra.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	printf("M.Guru Raghavendra");
+	printf("\n20p35a0332");
+	printf("\tMechanical Engineering");
+}
---
+++ jayesh .c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	printf("M.Jayesh");
+	printf("\n20P35A0309");
+	printf("\tMechanical");
+}
