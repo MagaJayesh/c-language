7ca6ae5f23cbbdc6089c61af12387b6aa3da3dfb
Maga Jayesh <92910436+MagaJayesh@users.noreply.github.com>
2021-10-29 00:06:22 -0700

    Add files via upload

---
+++ data.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//Variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	//Initialization
+	age=17;
+	cgpa=8.0;
+	initial='M';
+	//output
+	printf("age %d\n",age);
+	printf("percentage %f\n",cgpa);
+	printf("initial %c",initial);
+}
