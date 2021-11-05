7c9c82bf7153663b585da0dc024899657b9a5227
Maga Jayesh <92910436+MagaJayesh@users.noreply.github.com>
2021-11-05 00:13:18 -0700

    Add files via upload

---
+++ header file programme.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<limits.h>
+void main()
+{
+	printf("short Signed\t: %hd to %hd",SHRT_MIN, SHRT_MAX);
+}
---
+++ rules for naming variables..c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+/*Rules for defining variables
+A variable can have alphabets, digits, and underscore.
+A variable name can start with the alphabet, and underscore only. It can’t start with a digit.
+No whitespace is allowed within the variable name.
+A variable name must not be any reserved word or keyword, e.g. int, goto , etc.*/
+     
+	       
+}
