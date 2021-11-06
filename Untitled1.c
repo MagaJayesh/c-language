704497ee1803bfdf501866ba3974c13435e7812e
Maga Jayesh <92910436+MagaJayesh@users.noreply.github.com>
2021-11-06 00:01:20 -0700

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<limits.h>
+void main()
+{
+	printf("short Signed\t: %hd to %hd",SHRT_MIN, SHRT_MAX);
+	printf("\nshort unsinged\t:%ud to %ud",0,USHRT_MAX);
+}
---
+++ oerations.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,s,x,y,z,p;
+	a=10,b=2,s=a+b,x=a-b,y=a*b,z=a/b,p=a%b;
+	printf("sum is %d \n subtraction is %d \n product is %d \n division is %d\n reminder is %d",s,x,y,z,p);
+}
